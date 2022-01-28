3f21a7ec0a2191ec9c27697e4aa212e244e661aa
DaryaTorbina <dasavelyeva@yandex.ru>
2022-01-28 18:21:46 +0300

    v2 пр5

--- script/script.js
+++ script/script.js
@@ -1,30 +1,3 @@
-const initialCards = [
-	{
-	  name: 'Архыз',
-	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-	},
-	{
-	  name: 'Челябинская область',
-	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-	},
-	{
-	  name: 'Иваново',
-	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-	},
-	{
-	  name: 'Камчатка',
-	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-	},
-	{
-	  name: 'Холмогорский район',
-	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-	},
-	{
-	  name: 'Байкал',
-	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-	}
-  ];
-
 //переменные для создания элемента
 const sectionElements = document.querySelector('.elements');
 const cardTemplate = document.querySelector('#element').content;
@@ -62,117 +35,102 @@ const closeZoomButton = popupZoomImage.querySelector('.popup__buttonClose')
 const popupZoomImageImg = popupZoomImage.querySelector('.popup__image')
 const popupZoomImageTitle = popupZoomImage.querySelector('.popup__description')
 
+//функции открытия и закрытия попапа
+function openPopup (popup) {
+	popup.classList.add('popup__open'); 
+} 
+
+function closePopup (popup) {
+	popup.classList.remove('popup__open'); 
+} 
+
+//редактирование профиля
+function editProfile(evt) {
+	evt.preventDefault();
+	profileName.textContent = inputProfileName.value;
+	profileAbout.textContent = inputProfileAbout.value;
+	closePopup(editModalProfile);
+}
+
+//открытие попапа профиля
+editProfileButton.addEventListener('click', () => {
+	openPopup(editModalProfile);
+	inputProfileName.value = profileName.textContent;
+	inputProfileAbout.value = profileAbout.textContent;
+});
+
+//клик по кнопке открытие попапа профиля
+editProfileButton.addEventListener('click',() => {
+		openPopup(editModalProfile);
+});
+
+//закрытие попапа профиля
+closeProfileButton.addEventListener('click',() => {
+	closePopup(editModalProfile);
+});
+
+//редактирование профиля
+editModalProfile.addEventListener('submit',editProfile);
+
+//действия с карточками
+//открытие попапа карточки 
+addMestoButton.addEventListener('click',() => {
+	openPopup(addModalMesto);
+	inputMestoName.value = ''; 
+	inputMestoLink.value = '';
+});
+
+//закрытие
+closeMestoButton.addEventListener('click',() => {
+	closePopup(addModalMesto);
+});
+
+//добавление
+function addMestoCard (evt) {
+	evt.preventDefault();
+	sectionElements.prepend(createNewCard(inputMestoName.value, inputMestoLink.value));
+	closePopup(addModalMesto);
+};
+
+addModalMesto.addEventListener('submit',addMestoCard);
+
 //Функция создания карточки элемента в ней- изображение, лайк, корзина удаление.
 function createNewCard (newName, newLink) {
-	const CardUserElement = cardTemplate.querySelector('.element').cloneNode(true); //получаем клонируем-копируем элемент вместе с содержимым но не добавляем
-	const CardElementImage = CardUserElement.querySelector('.element__image');
-	CardElementImage.src = newLink;  //записали ссылку и на место встало изображение из массива
+	const cardUserElement = cardTemplate.querySelector('.element').cloneNode(true); //получаем клонируем-копируем элемент вместе с содержимым но не добавляем
+	const cardElementImage = cardUserElement.querySelector('.element__image');
+	cardElementImage.src = newLink;  //записали ссылку и на место встало изображение из массива
 	//добавляем в него
-	CardUserElement.querySelector('.element__name').textContent = newName; //записали имя в карточку
-	CardElementImage.alt = newName;  //записали имя в .element__image alt внутри
-
-	const buttonLike = CardUserElement.querySelector('.element__like');
-	//buttonLike.addEventListener('click', like);
-	// function like() {
-	// 	buttonLike.classList.toggle('element__like_active');
-	// }
+	cardUserElement.querySelector('.element__name').textContent = newName; //записали имя в карточку
+	cardElementImage.alt = newName;  //записали имя в .element__image alt внутри
 
 	//лайк 
-	CardUserElement.querySelector('.element__like').addEventListener('click', evt => {
+	cardUserElement.querySelector('.element__like').addEventListener('click', evt => {
 		const buttonLike = evt.target;			//инициатор событитя
 		buttonLike.classList.toggle('element__like_active');
 	});
 
 	//удаление
-	CardUserElement.querySelector('.element__delete').addEventListener('click', () => {
-	CardUserElement.remove();
+	cardUserElement.querySelector('.element__delete').addEventListener('click', () => {
+	cardUserElement.remove();
 	});
 
 	//открытие попапа с картинкой
-	CardElementImage.addEventListener('click', () => {
-		toggleModal(popupZoomImage);
+	cardElementImage.addEventListener('click', () => {
+		openPopup(popupZoomImage);
 		popupZoomImageImg.src = newLink;
 		popupZoomImageTitle.textContent = newName;
+		popupZoomImageImg.alt = newName;
+
 	});
-	return CardUserElement;
+	return cardUserElement;
 }
 
-//обощаем функции модалок
-function toggleModal(modal) {
-	modal.classList.toggle('popup__open')
-}
-// //пробуем заемнтить на функцию тоггл 1 2
-// function toggleEditModalProfile() {
-// 	editModalProfile.classList.toggle('popup__open')
-// }
-
-// //пробуем заемнтить на функцию тоггл 3 4
-// function toggleAddModalMesto() {
-// 	addModalMesto.classList.toggle('popup__open')
-// }
-
-editProfileButton.addEventListener('click',function() {
-	toggleModal(editModalProfile)
-})
-
-closeProfileButton.addEventListener('click',function() {
-	toggleModal(editModalProfile)
-})
-
-addMestoButton.addEventListener('click',function() {
-	toggleModal(addModalMesto)
-})
-
-closeMestoButton.addEventListener('click',function() {
-	toggleModal(addModalMesto)
-})
-
-closeZoomButton.addEventListener('click',function() {
-	toggleModal(popupZoomImage)
-})
-// //или стрелочной описать все 4
-// editProfileButton.addEventListener('click',() => toggleModal(editModalProfile))
-
-editFormProfile.addEventListener('submit',(evt) => {
-	evt.preventDefault()
-profileName.textContent = inputProfileName.value
-profileAbout.textContent = inputProfileAbout.value
-toggleModal(editModalProfile)
-})
-
-addFormMesto.addEventListener('submit',(evt) => {
-	evt.preventDefault();
-	sectionElements.prepend(createNewCard(inputMestoName.value, inputMestoLink.value));
-
-	toggleModal(addModalMesto)
+//увеличение фото-карточки закрытие
+closeZoomButton.addEventListener('click',() => {
+	closePopup(popupZoomImage);
 })
 
 //добавление массива элементов при загрузке страницы
 initialCards.forEach(card => {
 	sectionElements.append(createNewCard (card.name, card.link)); //добавляем append
 });
-
-// //1 клик по кнопке открытие попапа профиля
-// editProfileButton.addEventListener('click',() => {
-// //console.log ('click', editModalProfile)
-// editModalProfile.classList.add ('popup__open')
-// })
-
-// //2 клик по кнопке закрытие попапа профиля
-// closeProfileButton.addEventListener('click',() => {
-// 	//console.log ('click', editModalProfile)
-// 	editModalProfile.classList.remove ('popup__open')
-// 	})
-	
-
-// //3 клик по кнопке открытие попапа mesto
-// addMestoButton.addEventListener('click',() => {
-// 	//console.log ('click', editModalProfile)
-// 	addModalMesto.classList.add ('popup__open')
-// 	})
-	
-// 	//4 клик по кнопке закрытие попапа mesto
-// 	closeMestoButton.addEventListener('click',() => {
-// 		//console.log ('click', editModalProfile)
-// 		addModalMesto.classList.remove('popup__open')
-// 		})
